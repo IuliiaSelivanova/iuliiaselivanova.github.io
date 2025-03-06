e000b5162bb0def5dcef4c288adf921ae1bb4095
IuliiaSelivanova <yulya.danilina.96@inbox.ru>
2025-03-06 22:58:47 +0300

    Добавлена секция AboutMe

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import iconDownload from "../assets/icons/download-icon.svg";
+import mainPortret from "../assets/images/cat1.png";
 
 const AboutMe = () => {
-  return <div>About</div>;
+  return (
+    <main className="about mt-4">
+      <div className="container">
+        <div className="row align-items-center flex-column flex-md-row-reverse">
+          <div className="col-12 col-md d-flex justify-content-center about__img">
+            <img
+              src={mainPortret}
+              alt="portret"
+              className="img-fluid"
+            />
+          </div>
+
+          <div className="col-12 col-md">
+            <span className="subtitle">
+              Frontend-разработчик
+            </span>
+            <h1 className="title">
+              Привет, меня зовут Юлия Селиванова
+            </h1>
+            <p className="text">
+              Я начинающий frontend разработчик, который
+              строит новую карьеру на том, что мне
+              действительно нравится. В течение года я
+              оттачивала свои навыки веб-разработчика,
+              изучая, как создавать веб-приложения с
+              использованием HTML, CSS, JavaScript, React.
+            </p>
+            <button className="about__btn btn btn-warning ">
+              Резюме
+              <img src={iconDownload} alt="icon download" />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <Skills /> */}
+    </main>
+  );
 };
 
 export default AboutMe;
